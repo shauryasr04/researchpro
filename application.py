04e8f80f654e17fe6977a22c8efcae25e9d1467a
shauryasr04 <shauryasr059@gmail.com>
2021-01-02 18:48:15 -0500

    round II

--- application.py
+++ application.py
@@ -1,22 +1,23 @@
 # application.py
-from flask import Flask
+from flask import Flask, url_for
 from markupsafe import escape
+
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "<h1>Hello World! Shaurya \n How are you all doing?</h1>"
+@app.route('/')
+def index():
+    return 'index'
+
+@app.route('/login')
+def login():
+    return 'login'
+
 @app.route('/user/<username>')
-def show_user_profile(username):
-    # show the user profile for that user
-    return 'User %s' % escape(username)
+def profile(username):
+    return '{}\'s profile'.format(escape(username))
 
-@app.route('/post/<int:post_id>')
-def show_post(post_id):
-    # show the post with the given id, the id is an integer
-    return 'Post %d' % post_id
-
-@app.route('/path/<path:subpath>')
-def show_subpath(subpath):
-    # show the subpath after /path/
-    return 'Subpath %s' % escape(subpath)
+with app.test_request_context():
+    print(url_for('index'))
+    print(url_for('login'))
+    print(url_for('login', next='/'))
+    print(url_for('profile', username='John Doe'))
